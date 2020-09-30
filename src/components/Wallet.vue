a32a981735b8120a3e5f437008ec041387bb27bf
FallenGravity <ethanvb55@gmail.com>
2020-09-30 17:08:59 +0200

    Update Wallet.vue with new coins

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="col-lg-5">
       <div class="tab-content-inner">
-        <div class="heading mb-30">The Ether-1 Mobile Wallet allows users to securely store their $ETHO, $ATH & $XERO on their iOS/iPadOS device. <br> <br> Users can transact $ETHO, $ATH, $EGEM & $XERO between each other quickly and discretely by using their address or QR code. Users can also store their $ETHO, $ATH, $EGEM & $XERO and view their current holdings in the currency of their choice.</div>
+        <div class="heading mb-30">The Ether-1 Mobile Wallet allows users to securely store their $ETHO, $BSC, $ETH, $POA, $ATH, $EGEM, $XDAI & $XERO on their iOS/iPadOS device. <br> <br> Users can transact $ETHO, $BSC, $ETH, $POA, $ATH, $EGEM, $XDAI & $XERO between each other quickly and discretely by using their address or QR code. Users can also store their $ETHO, $ATH, $EGEM & $XERO and view their current holdings in the currency of their choice.</div>
         <div class="tab-button mb-30">
           <a href="https://apps.apple.com/za/app/nextshore-ether-1-wallet/id1521802485" class="ht-btn ht-btn-md">iOS Version </a>
           <a href="https://play.google.com/store/apps/details?id=app.web.nextshore_ether1" style="margin-left: 5px;" class="ht-btn ht-btn-md">Android Version</a>
