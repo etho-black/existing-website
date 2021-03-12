0a0bb4aadbd48602acc1bf843ea5bc5819544a4a
Dev-JamesR <admin@ether1.org>
2021-03-12 22:14:21 +0000

    Misc Front Page Updates

--- src/components/TeamOne.vue
+++ src/components/TeamOne.vue
@@ -73,6 +73,65 @@
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="grid-item">
+          <div class="ht-team-member text-center">
+            <div class="team-image">
+              <img class="img-fluid" src="../assets/img/team/exlo.png" alt="Exlo">
+            </div>
+            <div class="team-info text-center">
+              <h5 class="name">{{ $t('team.dev.exlo') }}</h5>
+              <div class="position">{{ $t('team.dev.exlo-role') }}</div>
+              <div class="social-networks">
+                <div class="inner">
+                  <a href="https://github.com/Exlo84" class=" hint--bounce hint--top  hint--primary" aria-label="Github"><i class="fab fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="grid-item">
+          <div class="ht-team-member text-center">
+            <div class="team-image">
+              <img class="img-fluid" src="../assets/img/team/avatar-05.png" alt="Legacytrx">
+            </div>
+            <div class="team-info text-center">
+              <h5 class="name">{{ $t('team.dev.legacy') }}</h5>
+              <div class="position">{{ $t('team.dev.exlo-role') }}</div>
+              <div class="social-networks">
+                <div class="inner">
+                  <a href="https://github.com/Legacytrx" class=" hint--bounce hint--top  hint--primary" aria-label="Github"><i class="fab fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="grid-item">
+          <div class="ht-team-member text-center">
+            <div class="team-image">
+              <img class="img-fluid" src="../assets/img/team/avatar-05.png" alt="newCodeRunner">
+            </div>
+            <div class="team-info text-center">
+              <h5 class="name">{{ $t('team.dev.newCodeRunner') }}</h5>
+              <div class="position">{{ $t('team.dev.exlo-role') }}</div>
+              <div class="social-networks">
+                <div class="inner">
+                  <a href="https://github.com/newCodeRunner" class=" hint--bounce hint--top  hint--primary" aria-label="Github"><i class="fab fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </template>
